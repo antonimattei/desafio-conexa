e2f7166496f64da6c8e08361ae11de897a3e5dda
Antoni Mattei <antonidemedeiros@gmail.com>
2022-12-02 20:18:00 -0300

    initial commit

---
+++ src/main/java/com/desafio/api/DesafioConexaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioConexaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioConexaBackendApplication.class, args);
+	}
+
+}
